bc4daf11fd9a2ba2a7814f28635d881a2c11a04b
Arthur <arthur@studio.gd>
2022-08-27 14:48:14 -0400

    init project

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@material-tailwind/react'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './routes/App'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
